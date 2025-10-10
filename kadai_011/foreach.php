2d8470a7bdadfe2c469e75e2a16de8bcb2faa1d6
hiurasaki31-lab <kudousaki31@gmail.com>
2025-10-10 14:37:41 +0900

    kadai_011をアップロード

---
+++ kadai_011/foreach.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang ="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>foreach構文課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $vegetables = ['名前' => '玉ねぎ','値段' => '200','産地' => '北海道'];
+
+    foreach($vegetables as $key => $value) {
+      echo"{$key} : {$value}" . '<br>';
+    }
+    ?>
+  </p>
+</body>
